ea4f78fe5d6fd181829bab501bbc8afbc6a01830
Akisa-Moruri <akisamoruri@gmail.com>
2024-04-12 21:16:24 +0300

    initialised changes

--- main.js
+++ main.js
@@ -4,13 +4,34 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  const likeButtons = document.getElementsByClassName("like");
+
+  for (const button of likeButtons) {
+    button.addEventListener("click", function() {
+      mimicServerCall()
+        .then(() => {
+          if (button.textContent === EMPTY_HEART) {
+            button.textContent = FULL_HEART;
+            button.classList.add("activated-heart");
+          } else {
+            button.textContent = EMPTY_HEART;
+            button.classList.remove("activated-heart");
+          }
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+    });
+  }
+});
+
 
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
-
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
